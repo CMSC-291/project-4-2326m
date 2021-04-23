cfa233629b39fce69c8d3f2cee9419e07e634181
Adam <67239620+2326m@users.noreply.github.com>
2021-04-23 14:04:36 -0400

    server runs as expected

--- app/settings.py
+++ app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x!18sh0bcap=09$&pcx3^t$x78tb3+55iq^i+max+pr&=wmadb'
+SECRET_KEY = 'django-insecure-$r1$sbxvq(2(ya8hs^l4b&!2+&=mf645oswyk^4h6169t8ls=m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
